aa55619d697445f163650b4e402d96f638c5d987
Matthieu Ancellin <matthieu.ancellin@ens-paris-saclay.fr>
2020-03-13 19:31:32 +0100

    Fix signed distance of PolarHalfPlane.

--- test/test_halfplanes.jl
+++ test/test_halfplanes.jl
@@ -1,6 +1,6 @@
 using Test
-using PolygonArea: PolarHalfPlane, angle
-using PolygonArea: Point, distance, corner, Intersection, Reunion
+using PolygonArea
+using PolygonArea: PolarHalfPlane, Point, corner, Intersection, Reunion
 using StaticArrays
 
 inferior_hp = HalfPlane(0, 1, 0)
@@ -23,29 +23,32 @@ right_hp = HalfPlane(-1, 0, 0)
 @test PolygonArea.corner(inferior_hp, right_hp) == @SVector [0.0, 0.0]
 @test PolygonArea.corner(HalfPlane(1, 0, 1), HalfPlane(0, -1, 0.5)) == @SVector [-1.0, 0.5]
 
-@test distance(Point(9.0, 0.0), PolarHalfPlane(0.0, 0.0)) ≈ 9.0
-@test distance(Point(9.0, 0.0), PolarHalfPlane(-1.0, 0.0)) ≈ 10.0
-@test distance(Point(9.0, -10.0), PolarHalfPlane(-1.0, 0.0)) ≈ 10.0
-@test distance(Point(9.0, 1.0), PolarHalfPlane(0.0, π/2)) ≈ 1.0
-@test distance(Point(1.0, 1.0), PolarHalfPlane(0.0, π/4)) ≈ √2
-@test distance(Point(2.0, 2.0), PolarHalfPlane(√2, π/4)) ≈ √2
-@test distance(Point(6.0, 1.0), PolarHalfPlane(0.0, π/4, center=Point(5.0, 0.0))) ≈ √2
+@test PolygonArea.distance(Point(9.0, 0.0), PolarHalfPlane(0.0, 0.0)) ≈ 9.0
+@test PolygonArea.distance(Point(9.0, 0.0), PolarHalfPlane(1.0, 0.0)) ≈ 10.0
+@test PolygonArea.distance(Point(9.0, -10.0), PolarHalfPlane(1.0, 0.0)) ≈ 10.0
+@test PolygonArea.distance(Point(9.0, 1.0), PolarHalfPlane(0.0, π/2)) ≈ 1.0
+@test PolygonArea.distance(Point(1.0, 1.0), PolarHalfPlane(0.0, π/4)) ≈ √2
+@test PolygonArea.distance(Point(2.0, 2.0), PolarHalfPlane(-√2, π/4)) ≈ √2
+@test PolygonArea.distance(Point(6.0, 1.0), PolarHalfPlane(0.0, π/4, center=Point(5.0, 0.0))) ≈ √2
 
-@test Point(0.5, 0.5) in PolarHalfPlane(1.0, 0π)
-@test Point(0.5, 0.5) in PolarHalfPlane(1.0, π/2)
-@test Point(0.5, 0.5) in PolarHalfPlane(1.0, π)
-@test Point(0.5, 0.5) in PolarHalfPlane(1.0, 3π/2)
-@test Point(1.0, 1.0) in PolarHalfPlane(1.0, 0π)
-@test Point(0.5, 2.0) in PolarHalfPlane(1.0, 0π)
-@test Point(2.0, 0.5) in PolarHalfPlane(1.0, π/2)
-@test Point(0.0, 3.0) in PolarHalfPlane(1.0, π)
+@test Point(0.5, 0.5) in PolarHalfPlane(-1.0, 0π)
+@test Point(0.5, 0.5) in PolarHalfPlane(-1.0, π/2)
+@test Point(0.5, 0.5) in PolarHalfPlane(-1.0, π)
+@test Point(0.5, 0.5) in PolarHalfPlane(-1.0, 3π/2)
+@test Point(1.0, 1.0) in PolarHalfPlane(-1.0, 0π)
+@test Point(0.5, 2.0) in PolarHalfPlane(-1.0, 0π)
+@test Point(2.0, 0.5) in PolarHalfPlane(-1.0, π/2)
+@test Point(0.0, 3.0) in PolarHalfPlane(-1.0, π)
 
 @test corner(PolarHalfPlane(0.0, 0), HalfPlane(0.0, 1.0, -1.0)) == Point(0.0, 1.0)
-@test corner(PolarHalfPlane(1.0, π/2, center=Point(0.5, 0.5)), HalfPlane(1.0, 0.0, -1.0)) == Point(1.0, 1.5)
+@test corner(PolarHalfPlane(-1.0, π/2, center=Point(0.5, 0.5)), HalfPlane(1.0, 0.0, -1.0)) == Point(1.0, 1.5)
 
-@test angle(PolarHalfPlane(0.0, π/8)) ≈ π/8
-@test angle(PolarHalfPlane(0.0, -π/8)) ≈ mod(-π/8, 2π)
-@test angle(PolarHalfPlane(4.0, π/3)) ≈ π/3
+@test PolygonArea.angle(PolarHalfPlane(0.0, π/8)) ≈ π/8
+@test PolygonArea.angle(PolarHalfPlane(0.0, -π/8)) ≈ mod(-π/8, 2π)
+@test PolygonArea.angle(PolarHalfPlane(4.0, π/3)) ≈ π/3
+
+@test PolygonArea.signed_distance(Point(0.0, 0.0), PolarHalfPlane(-4.0, 2π/3)) ≈ -4.0
+@test PolygonArea.signed_distance(Point(0.0, 0.0),PolarHalfPlane(4.0, π/3)) ≈ 4.0
 
 # OLD TESTS
 
@@ -57,7 +60,7 @@ right_hp = HalfPlane(-1, 0, 0)
 #= @test invert_x_and_y(PolarHalfPlane(1.0, 0.0)) == PolarHalfPlane(1.0, π/2) =#
 #= @test invert_x_and_y(PolarHalfPlane(0.0, 0.0, 1.0, 0.0)) == PolarHalfPlane(0.0, π/2, 0.0, 1.0) =#
 
-h1 = HalfPlane(0, 0, 0)
-(h1 ∩ h1) ∪ (h1 ∩ h1)
-invert((h1 ∪ h1) ∩ (h1 ∪ h1))
-invert((h1 ∪ h1) ∩ (h1 ∪ h1))
+#= h1 = HalfPlane(0, 0, 0) =#
+#= (h1 ∩ h1) ∪ (h1 ∩ h1) =#
+#= invert((h1 ∪ h1) ∩ (h1 ∪ h1)) =#
+#= invert((h1 ∪ h1) ∩ (h1 ∪ h1)) =#
