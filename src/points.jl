9d2d3e498e036fc169a9d43c77f63d53f3fcea00
Matthieu Ancellin <matthieu.ancellin@ens-paris-saclay.fr>
2020-05-15 13:23:16 +0200

    Fix rotation with center given as Tuple.

--- src/points.jl
+++ src/points.jl
@@ -3,5 +3,5 @@ const Point = SVector{2, Float64}
 
 rotation_matrix(ϕ) = @SMatrix [cos(ϕ) -sin(ϕ); sin(ϕ) cos(ϕ)]
 
-rotate(p::Point, ϕ; center=Point(0.0, 0.0)) = center + rotation_matrix(ϕ) * (p - center)
+rotate(p::Point, ϕ; center=Point(0.0, 0.0)) = (center = Point(center...); center + rotation_matrix(ϕ) * (p - center))
 translate(p::Point, v) = p + v
--- test/test_points.jl
+++ test/test_points.jl
@@ -7,5 +7,6 @@ using PolygonArea: Point, rotate, translate
     p = Point(1.0, 0.0)
     @test rotate(p, π/2) ≈ Point(0.0, 1.0)
     @test rotate(p, π/2, center=Point(2.0, 0.0)) ≈ Point(2.0, -1.0)
+    @test rotate(p, π/4, center=(1.0, 0.0)) == Point(1.0, 0.0)
     @test translate(p, SVector(1.0, 0.0)) ≈ Point(2.0, 0.0)
 end
