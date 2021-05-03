2104cbf8be0a7db376a8d82a7c6fbdb9e82b29de
Matthieu Ancellin <matthieu.ancellin@ens-paris-saclay.fr>
2021-05-03 14:32:18 +0200

    Add method to rescale also points.

--- src/points.jl
+++ src/points.jl
@@ -7,3 +7,6 @@ rotate(p::Point, ϕ; center=Point{Float64}(0.0, 0.0)) = (center = Point(center[1
                                                         center + rotation_matrix(ϕ) * (p - center))
 
 translate(p::Point, v) = Point(p[1] + v[1], p[2] + v[2])
+
+scale(x::Point, λ::AbstractVector, center=Point(0.0, 0.0)) where T = λ.*(x.-center) .+ center
+scale(x::Point, λ::Number, center=Point(0.0, 0.0)) where T = scale(x, SVector{2}(λ, λ), center)
--- src/polygons.jl
+++ src/polygons.jl
@@ -20,8 +20,7 @@ isempty(p::ConvexPolygon) = length(p.vertices) <= 2
 
 translate(c::ConvexPolygon{T}, v) where T = ConvexPolygon{T}(map(x -> translate(x, v), c.vertices))
 rotate(c::ConvexPolygon{T}, ϕ; kw...) where T = ConvexPolygon{T}(map(x -> rotate(x, ϕ; kw...), c.vertices))
-scale(c::ConvexPolygon{T}, λ::AbstractVector, center=Point(0.0, 0.0)) where T = ConvexPolygon{T}(map(x -> λ.*(x.-center) .+ center, c.vertices))
-scale(c::ConvexPolygon{T}, λ::Number, center=Point(0.0, 0.0)) where T = scale(c, SVector{2}(λ, λ), center)
+scale(c::ConvexPolygon{T}, λ, center=Point(0.0, 0.0)) where T = ConvexPolygon{T}(map(x -> scale(x, λ, center), c.vertices))
 
 function area(c::ConvexPolygon{T}) where T
 	if isempty(c)
