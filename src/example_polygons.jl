2d6163ee4ed96ea4657ddec50a9ee987e472dc71
Matthieu Ancellin <matthieu.ancellin@ens-paris-saclay.fr>
2021-09-13 15:32:57 +0200

    Add more ways to generate circles.

--- src/example_polygons.jl
+++ src/example_polygons.jl
@@ -22,3 +22,18 @@ end
 
 circle(x0, y0, r, nb_vertices) = circle(promote(x0, y0, r)..., nb_vertices)
 circle(center::Union{Point, Tuple}, r, nb_sides) = circle(center[1], center[2], r, nb_sides)
+
+function circle(center::Point, point::Point, nb_vertices::Int)
+    radius = sqrt((point1[1] - center[1])^2 + (point1[2] - center[2])^2)
+    return circle(center, radius, nb_vertices)
+end
+
+function circle(point1::Point, point2::Point, curvature::Real, nb_vertices::Int)
+    radius = 1.0/curvature
+    distance_between_points = sqrt((point2[1] - point1[1])^2 + (point2[2] - point1[2])^2)
+    n = (point2 - point1) / distance_between_points
+    on_median = sqrt(radius^2 - (distance_between_points/2)^2)
+    n_ortho = SVector(n[2], -n[1])
+    center = point1 + distance_between_points/2 * n + on_median * n_ortho
+    return circle(center, radius, nb_vertices)
+end
